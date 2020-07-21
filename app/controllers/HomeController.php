d76570d8674a1a8a24099b6613b06a25bba5391f
dimtymur <63157948+dimtymur@users.noreply.github.com>
2020-07-22 00:17:54 +0300

    separate commit

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+
+class HomeController extends Controller {
+    public function index(string $arg1="", string $arg2="") : void {
+        $this->record_add("some_table", ["x" => 1, "y" => 2, "z" => 3]);
+        self::view("home.index", ["arg1" => $arg1, "arg2" => $arg2]);
+    }
+}
---
+++ inc/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Core\Route;
+use App\Core\Controller;
+
+Route::setRoute("/some/{}/loc/{}", "HomeController@index");
+
+Route::setRoute("/some/{}/path/{}", function(string $arg1, string $arg2) : void {
+    echo $arg1 . " " . $arg2;
+});
+
+Route::setRoute("/some/{}/path/{}/here", function(string $arg1, string $arg2) : void {
+    Controller::view("home.index", ["arg1" => $arg1, "arg2" => $arg2]);
+});
+
+Route::setRoute("/some/cool/path", function() : void {
+    echo "hello there";
+});
